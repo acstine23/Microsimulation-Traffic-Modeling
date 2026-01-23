5b77da3e584c49886ec0b96832405900ab6a803a
MyUnoriginalName-dsci <rachelgthephoenix@gmail.com>
2026-01-22 22:07:58 -0500

    Exploring velocity and road type (SECTION 2, eda_rachel)

--- eda_rachel.py
+++ eda_rachel.py
@@ -7,6 +7,7 @@ instances_df = pd.read_csv('Instances.csv')
 radar_points_df = pd.read_csv('Radar_Points.csv')
 runs_df = pd.read_csv('Runs.csv')
 
+""" SECTION 1 """
 # Q1: Does gender of driver impact results? Do men drive faster? Women?
 # Q2: Is the type of vehicle significant?
 
@@ -40,3 +41,23 @@ px.histogram(joined_radar_instance_run_df,x='congestion',color='sex').show()
     # 37,146 more radar points than recorded by men. Women made up over half of the observations 
     # recorded for congested traffic, while male and female drivers were nearly evenly responsible
     # for observations in uncongested traffic.
+
+""" SECTION 2 """
+# Using PowerBI to draft visualizations, what can we hypothesize about road_type and safety?
+
+''' 
+when we compare the variance and standard deviation in follower_vel and leader_vel with the road_type, 
+we find that there is a greater variation in velocity in instances where the road_type is aw (advanced warning) 
+and wz2 (work zone without lane closure). 
+
+This may indicate that not all drivers adjust their acceleration until A) they reach the work zone (tz)
+or B) a lane closure restricts their driving options (wz1).
+
+Follow up q's:
+- how significant is the variance of velocity / speed?
+- what is the distribution of velocity / acceleration for each road type?
+- how long does it take a vehicle to pass through the work zone in congested traffic? uncongested? with lane closure? without?
+    - is there a real difference in time or a perceived difference in time to traverse the work zone?
+
+
+'''
